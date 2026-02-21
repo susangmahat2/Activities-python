d9f11478a6ac03f24abd2937897cbd06da000f5d
susangmahat2 <susangmahat2@gmail.com>
2026-02-21 20:52:10 +0900

    for loop 3

--- activity3.py
+++ activity3.py
@@ -1,29 +1,8 @@
-print("select your ride")
-print("1. bike")
-print("2. car")
+num=int(input("Enter a number: "))
+sum=0
+for i in range(num,0,-1):
+    sum=sum+i
+    
+    print(i)
 
-choice=int(input("enter your choice: "))
-if choice==1:
-    print("what type of bike do you want?")
-    print("1. Scooty\n")
-    print("2.scooter\n")
-
-    choosebike=int(input("enter your choice: "))
-    if choosebike==1:
-        print("you have selected scooty")
-    elif choosebike==2:
-        print("you have selected scooter")
-    else:
-        print("invalid choice")
-elif choice==2:
-    print("what type of car do you want?")
-    print("1. sedan\n")
-    print("2. suv\n")
-
-    choice3=int(input("enter your choice: "))
-    if choice3==1:
-        print("you have selected sedan")
-    elif choice3==2:
-        print("you have selected suv")
-    else:
-        print("invalid choice")
+print("The sum of numbers from", num, "to 1 is:", sum)
