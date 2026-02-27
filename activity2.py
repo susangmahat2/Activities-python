1fc2441068404997eb12e74166f6737ba12b4344
susangmahat2 <susangmahat2@gmail.com>
2026-02-27 20:45:34 +0900

    nested loop prime numbers

--- activity2.py
+++ activity2.py
@@ -1,6 +1,11 @@
-while True:
-    choice=input("enter 'q' to quit")
-    if choice=='q':
-        
-        print("loop stopped")
-        
+lower=int(input("enter the lower number: "))
+upper=int(input("enter the upper number: "))
+print ("The prime numbers between",lower,"and",upper,"are:")
+for num in range (lower,upper+1):
+    if num>1:
+        for i in range(2,num):
+            if num%i==0:
+                break
+            else:
+                print(num)
+                break
