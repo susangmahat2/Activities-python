78648a8d710c85928ea17a0dce932484191e5c86
susangmahat2 <susangmahat2@gmail.com>
2026-02-27 20:28:29 +0900

    nested loop

--- activity1.py
+++ activity1.py
@@ -1,8 +1,8 @@
-n=int(input("Entre a natural number"))
-sum=0
-i=1
-while i<=n:
-    sum=sum+i
-    i=i+1
-print("The sum of first",n,"natural numbers is",sum)
-    
+word=input("Enter a word: ")
+char=input("Enter a character: ")
+
+count=0
+for i in word:
+    if i==char:
+        count+=1
+print("The character",char,"appears",count,"times in the word",word)
